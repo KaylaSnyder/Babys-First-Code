d2558e3ee16074830f99d40c8566638fee3f7dd0
KaylaSnyder <KaylaJSnyder@yahoo.com>
2022-07-10 20:53:20 -0500

    Removed Extra Folders

---
+++ Practise/script.js
@@ -0,0 +1,15 @@
+// i hate being poor
+
+// var answer = prompt"How many dogs?"
+response();
+
+function response() {
+    let taco = prompt("How many dogs?")
+    if ( taco == 1) {
+        document.getElementById("footer").innerHTML = "That's not enough"
+    }    
+    else (
+        console.log("I love that for you")
+    )
+}
+

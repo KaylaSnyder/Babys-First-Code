488f224b38fc84d943c997ff4256990fc524c201
kaylasnyder <kaylajsnyder@yahoo.com>
2022-07-20 21:11:53 -0500

    rad stuff

--- Practise/script.js
+++ Practise/script.js
@@ -3,13 +3,13 @@
 // var answer = prompt"How many dogs?"
 response();
 
-function response() {
-    let taco = prompt("How many dogs?")
-    if ( taco == 1) {
-        document.getElementById("footer").innerHTML = "That's not enough"
-    }    
-    else (
-        console.log("I love that for you")
-    )
-}
+// function response() {
+//     let taco = prompt("How many dogs?")
+//    if ( taco == 1) {
+//        document.getElementById("footer").innerHTML = "That's not enough"
+//     }    
+//     else (
+//        console.log("I love that for you")
+//    )
+// }
 
